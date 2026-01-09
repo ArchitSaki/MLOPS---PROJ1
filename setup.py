f20eedcb22cfdb576285ff78c57e7d6f8ca466bf
archit <itsarchit.saki@gmail.com>
2026-01-09 16:46:01 +0530

    project template and mongodb connection done

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Archit Saki",
+    author_email="itsarchit.saki@gmail.com",
+    packages=find_packages()
+)
